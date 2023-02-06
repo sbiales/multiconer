5c5544e8eab30235112d1677a41e026064db7128
Siena Biales <sienab1@gmail.com>
2023-02-06 11:32:34 +0100

    Add constants for separated model labels

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from models.constants import NER_LABEL2ID, NER_ID2LABEL, MULTI_LABEL2ID, MULTI_ID2LABEL, LANGCODES
+from models.constants import NER_LABEL2ID, NER_ID2LABEL, POS_LABEL2ID, POS_ID2LABEL, DEP_LABEL2ID, DEP_ID2LABEL, MULTI_LABEL2ID, MULTI_ID2LABEL, LANGCODES
 from models.multitask import MultitaskModel, NLPDataCollator, StrIgnoreDevice, DataLoaderWithTaskname, MultitaskDataloader, MultitaskTrainer
 from models.multitask import convert_to_ner_features, convert_to_pos_features, convert_to_dep_features
--- models/constants.py
+++ models/constants.py
@@ -9,6 +9,23 @@ NER_LABEL2ID = { 'O': 0, 'B-Station': 1, 'I-Station': 2, 'B-Facility': 3, 'I-Fac
 
 NER_ID2LABEL = { i:c for c,i in NER_LABEL2ID.items() }
 
+POS_LABEL2ID = { 'CCONJ': 0, 'AUX': 1, 'DET': 2, 'VERB': 3, 'SYM': 4, 'INTJ': 5, 'PROPN': 6, 'ADJ': 7, 'NOUN': 8, 'PART': 9, 'X': 10, 'NUM': 11, 'SCONJ': 12, 'ADV': 13, 'PUNCT': 14,
+  'ADP': 15, 'PRON': 16 }
+
+POS_ID2LABEL = { i:c for c,i in POS_LABEL2ID.items() }
+
+DEP_LABEL2ID = { 'obj': 0, 'vocative': 1, 'acl:cleft': 2, 'conj:svc': 3, 'compound': 4, 'advcl': 5, 'acl:relcl': 6, 'vocative:mention': 7, 'advmod:det': 8, 'iobj:agent': 9,
+ 'xcomp': 10, 'aux': 11, 'nmod:poss': 12, 'clf': 13, 'compound:prt': 14, 'obl:mod': 15, 'obl:npmod': 16, 'obl:patient': 17, 'advcl:svc': 18, 'discourse:emo': 19, 'nummod:gov': 20,
+ 'expl:impers': 21, 'nsubj': 22, 'advcl:cleft': 23, 'appos': 24, 'acl:adv': 25, 'amod': 26, 'nsubj:caus': 27, 'case': 28, 'parataxis:appos': 29, 'mark:rel': 30, 'flat': 31,
+ 'ccomp': 32, 'expl:pv': 33, 'flat:num': 34, 'acl': 35, 'expl': 36, 'orphan': 37, 'det:predet': 38, 'aux:caus': 39, 'mark:adv': 40, 'flat:title': 41, 'det:numgov': 42, 'obj:lvc': 43,
+ 'expl:pass': 44, 'parataxis:discourse': 45, 'det:nummod': 46, 'flat:foreign': 47, 'expl:subj': 48, 'parataxis': 49, 'det': 50, 'reparandum': 51, 'det:poss': 52, 'compound:lvc': 53,
+ 'conj': 54, 'mark': 55, 'cc:preconj': 56, 'cop': 57, 'discourse': 58, 'flat:range': 59, 'iobj': 60, 'nmod': 61, 'dep': 62, 'aux:pass': 63, 'discourse:sp': 64, 'obl:arg': 65,
+ 'obl': 66, 'obj:agent': 67, 'obl:tmod': 68, 'fixed': 69, 'root': 70, 'compound:ext': 71, 'flat:repeat': 72, 'dislocated': 73, 'goeswith': 74, 'flat:sibl': 75, 'flat:name': 76,
+ 'list': 77, 'obl:agent': 78, 'nsubj:pass': 79, 'aux:tense': 80, 'advcl:sp': 81, 'punct': 82, 'nummod': 83, 'advmod': 84, 'xcomp:sp': 85, 'flat:abs': 86, 'nmod:npmod': 87,
+ 'cc': 88, 'dep:comp': 89, 'nmod:tmod': 90, 'csubj': 91, 'csubj:pass': 92 }
+
+DEP_ID2LABEL = { i:c for c,i in DEP_LABEL2ID.items() }
+
 MULTI_LABEL2ID = { 'O': 0, 'B-Station': 1, 'I-Station': 2, 'B-Facility': 3, 'I-Facility': 4, 'B-HumanSettlement': 5, 'I-HumanSettlement': 6, 'B-OtherLOC': 7, 'I-OtherLOC': 8,
  'B-Symptom': 9, 'I-Symptom': 10, 'B-Medication/Vaccine': 11, 'I-Medication/Vaccine': 12, 'B-MedicalProcedure': 13, 'I-MedicalProcedure': 14, 'B-AnatomicalStructure': 15,
  'I-AnatomicalStructure': 16, 'B-Disease': 17, 'I-Disease': 18, 'B-Clothing': 19, 'I-Clothing': 20, 'B-OtherPROD': 21, 'I-OtherPROD': 22, 'B-Vehicle': 23, 'I-Vehicle': 24,
